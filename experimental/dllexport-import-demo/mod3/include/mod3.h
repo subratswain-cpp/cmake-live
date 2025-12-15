150bc9312686238e89c9345c171083b01119cd06
subratswain-cpp <cppwithsubrat@gmail.com>
2025-12-15 16:47:00 +0530

    Make mod-3(Experimental) cross-platform compatible

--- experimental/dllexport-import-demo/mod3/include/mod3.h
+++ experimental/dllexport-import-demo/mod3/include/mod3.h
@@ -1,6 +1,13 @@
 #pragma once // It is not part of standard C++ as of now. But it is implemented in many compilers.
 
 namespace mod3 {
-    __declspec(dllexport) void fun();
-    __declspec(dllexport)  void myInternalFunc(); // TASK: remove __declspec(dllexport) and observe the error.
+    #if(WIN32) // TODO: Not a recommened way! We'll see the best approach in upcomming session.
+        __declspec(dllexport) void fun();
+        __declspec(dllexport)  void myInternalFunc(); // TASK: remove __declspec(dllexport) and observe the error.
+    #else
+        void fun();
+        void myInternalFunc();
+    #endif
+
+
 } // namespace mod3
